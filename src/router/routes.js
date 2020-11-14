eb7bef17ecaf6eb0a6c3d0ec0c2ce8dac8990557
HylJune16 <hyl06100715@163.com>
2020-11-14 17:04:24 +0800

    feat:购物车页

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,8 +1,9 @@
 // @ts-nocheck
-import Home from "../views/Home";
+/* import Home from "../views/Home"; */
+import addToCart  from '../pages/addToCart'
 // @ts-ignore
 const routes = [
-  { path: "/", component: Home },
+  { path: "/", component: addToCart },
   // { path: "/about", component: () => import("../views/About") },
 ];
 export default routes;
