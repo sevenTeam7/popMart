6e1ad189c69c6d557e6742c7a0c951ada6a8c363
Hopeliusong <hopeliusong@163.com>
2020-11-14 16:53:37 +0800

    feat:header公共组件

--- src/main.js
+++ src/main.js
@@ -1,9 +1,13 @@
 import Vue from "vue";
+import ElementUI from "element-ui";
+import "element-ui/lib/theme-chalk/index.css";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import "./styles/index.less";
+import "./styles/reset.css";
 Vue.config.productionTip = false;
+Vue.use(ElementUI);
 
 // 导出所有的Api 挂载到vue写原型上
 import * as API from "./api";
--- src/router/routes.js
+++ src/router/routes.js
@@ -1,8 +1,10 @@
 // @ts-nocheck
 import Home from "../views/Home";
+import Header from "../components/Header";
 // @ts-ignore
 const routes = [
   { path: "/", component: Home },
+  { path: "/header", component: Header },
   // { path: "/about", component: () => import("../views/About") },
 ];
 export default routes;
