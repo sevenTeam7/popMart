3969deb5ea1a16e4dabe1425671de114a823ca83
Yangxianpeng26 <yangxianoeng26>
2020-11-15 19:43:41 +0800

    完成home的静态dmdm

--- src/main.js
+++ src/main.js
@@ -5,6 +5,20 @@ import store from "./store";
 import "./styles/index.less";
 Vue.config.productionTip = false;
 
+//引入elemetUi
+import ElementUI from "element-ui";
+import "element-ui/lib/theme-chalk/index.css";
+
+//引入样式swpier
+import "swiper/css/swiper.css";
+
+
+
+Vue.use(ElementUI);
+
+//引入ui样式
+// import '@/'
+
 // 导出所有的Api 挂载到vue写原型上
 import * as API from "./api";
 Vue.prototype.$API = API;
--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -1,19 +1,394 @@
 <template>
-  <div>homme</div>
-</template>
+  <div>
+    <div class="container-box">
+      <div class="loader-container">
+        <!-- 头部北京图片 -->
+        <div class="header_bg">
+          <div class="header">
+            <a href="###"></a>
+            <div class="nav_con">
+              <a href="###" cl>关于我们</a>
+              <a href="###">新闻</a>
+              <a href="###">附近门店</a>
+              <a href="###">合作</a>
+              <a href="###">葩趣</a>
+              <a href="###">线上商城<i class="el-icon-arrow-down"></i></a>
+              <a href="###">海上市场</a>
+              <a href="###">海上平台<i class="el-icon-arrow-down"></i></a>
+            </div>
+            <div id="lang">
+              <a href="###" class="active">简</a>
+              <span class="line"></span>
+              <a href="###">繁</a>
+              <span class="line"></span>
+              <a href="###">EN</a>
+            </div>
+          </div>
+        </div>
+      </div>
+      <!-- 头部轮播图 -->
+      <div class="swiper-container">
+        <div class="swiper-wrapper">
+          <div class="swiper-slide">
+            <img src="./images/背景图片.jpg" />
+          </div>
+          <div class="swiper-slide">
+            <img src="./images/大黑图.png" />
+          </div>
+          <div class="swiper-slide">
+            <img src="./images/背景图片.jpg" />
+          </div>
+          <div class="swiper-slide">
+            <img src="./images/背景图片.jpg" />
+          </div>
+        </div>
+        <!-- 如果需要分页器 -->
 
+        <div id="swiper-pagination" class="swiper-pagination"></div>
+        <!-- 如果需要导航按钮 -->
+        <div id="swiper-button-prev" class="swiper-button-prev"></div>
+        <div id="swiper-button-next" class="swiper-button-next"></div>
+      </div>
+
+      <!-- 视频vidio -->
+      <div class="video_con">
+        <div class="video_tltle">
+          ABOUT US
+        </div>
+        <!-- 我们 -->
+        <div class="video_we">
+          关于我们
+        </div>
+        <p class="video_history">
+          北京泡泡玛特文化创意有限公司成立于 2010
+          年，是中国领先的潮流文化娱乐公司
+
+          <br />
+
+          发展近十年来，围绕艺术家挖掘、IP
+          孵化运营、消费者触达以及潮玩文化推广与培育四个领域
+
+          <br />
+
+          POP MART
+          泡泡玛特旨在用“创造潮流，传递美好”的品牌文化构建了覆盖潮流玩具全产业链的综合运营平台
+        </p>
+        <!-- 关于我们注意这里有个跳转到关于我们的页面 -->
+        <a href="###">More <i class="el-icon-arrow-right"></i></a>
+      </div>
+
+      <!-- 视频 -->
+
+      <div class="video_box">
+        <div class="video">
+          <video
+            src="https://popwebsite.paquapp.com/popmartwww/pc/index_video.mp4"
+            autoplay
+            controls
+            loop="loop"
+          ></video>
+        </div>
+      </div>
+
+      <!-- TO LIGHT UP PASSION AND BRING JOY -->
+      <div class="company_con">
+        <div class="en_f">TO LIGHT UP PASSION AND BRING JOY</div>
+        <p class="found">创造潮流 传递美好</p>
+        <ul class="company_con_item_box">
+          <a href="###" style="text-decoration: none;">
+            <li class="item">
+              <div class="magnify">
+                <img src="./images/index_company_icon4.png" alt="" />
+                <p class="item_title">设计师</p>
+              </div>
+              <p class="item_desc">
+                签约
+                Kenny、毕奇、龙家升等知名潮玩设计师，推出众多粉丝喜爱的潮玩产品
+              </p>
+            </li>
+          </a>
+          <a href="###" style="text-decoration: none;">
+            <li class="item">
+              <div class="magnify">
+                <img src="./images/index_company_icon3.png" alt="" />
+                <p class="item_title">设计师</p>
+              </div>
+              <p class="item_desc">
+                签约
+                Kenny、毕奇、龙家升等知名潮玩设计师，推出众多粉丝喜爱的潮玩产品
+              </p>
+            </li>
+          </a>
+          <a href="###" style="text-decoration: none;">
+            <li class="item">
+              <div class="magnify">
+                <img src="./images/index_company_icon2.png" alt="" />
+                <p class="item_title">设计师</p>
+              </div>
+              <p class="item_desc">
+                签约
+                Kenny、毕奇、龙家升等知名潮玩设计师，推出众多粉丝喜爱的潮玩产品
+              </p>
+            </li>
+          </a>
+          <a href="###" style="text-decoration: none;">
+            <li class="item">
+              <div class="magnify">
+                <img src="./images/index_company_icon1.png" alt="" />
+                <p class="item_title">设计师</p>
+              </div>
+              <p class="item_desc">
+                签约
+                Kenny、毕奇、龙家升等知名潮玩设计师，推出众多粉丝喜爱的潮玩产品
+              </p>
+            </li>
+          </a>
+        </ul>
+      </div>
+
+      <!-- 媒体新闻 -->
+      <div class="news_con">
+        <p class="news">NEWS</p>
+        <p class="journalism">媒体新闻</p>
+        <div class="news_item_con ">
+          <a
+            href="http://life.ynet.com/2020/10/12/2914725t978.html"
+            target="_blank"
+            class="news_item"
+          >
+            <img
+              src="https://popwebsite.paquapp.com/cms/news/6czx0Sr6063z1480BCT6t8bX2PycZi4b.jpeg"
+              alt=""
+            />
+            <div class="pop_1">
+              <div class="news_title">
+                做公仔，当然是开心最重要啦
+              </div>
+              <p class="news_desc">
+                设计师Ton Mak分享泡泡玛特“Magical
+                Natural”主题系列盲盒公仔的设计历程和艺术人生。
+              </p>
+              <a
+                href="http://life.ynet.com/2020/10/12/2914725t978.html"
+                class="el-icon-arrow-right"
+                >MORE</a
+              >
+            </div>
+          </a>
+          <a
+            href="https://cci.ifeng.com/c/80IDz2F0frM"
+            target="_blank"
+            class="news_item"
+          >
+            <img
+              src="https://popwebsite.paquapp.com/cms/news/jzC0132CM8Jfh6XB48bAsycTEa80Yirb.jpg"
+              alt=""
+            />
+            <div class="pop_1">
+              <div class="news_title">
+                MIGO设计师雯子：勇于表达自己的熊孩子很可爱
+              </div>
+              <p class="news_desc">
+                用心设计有魅力的作品，不断给玩家带来美好和快乐，期待与玩家的心灵交流，对她而言，同样也是一件美妙的体验。
+              </p>
+              <a
+                href="http://life.ynet.com/2020/10/12/2914725t978.html"
+                class="el-icon-arrow-right"
+                >MORE</a
+              >
+            </div>
+          </a>
+          <a
+            href="http://www.cnr.cn/rdzx/cxxhl/zxxx/20200930/t20200930_525283932.shtml"
+            target="_blank"
+            class="news_item"
+          >
+            <img
+              src="https://popwebsite.paquapp.com/cms/news/faz2E2592kdia7kQ0fNm03ZrM6rerHz0.png"
+              alt=""
+            />
+            <div class="pop_1">
+              <div class="news_title">
+                潮玩登录未来火星生活周，泡泡玛特让世界了解中国当代青年
+              </div>
+              <p class="news_desc">
+                9月30日，未来火星生活周正式开幕，泡泡玛特亮相甘肃金昌“火星1号基地”，用潮玩的方式搭建火星的生活场景。
+              </p>
+              <a
+                href="http://life.ynet.com/2020/10/12/2914725t978.html"
+                class="el-icon-arrow-right"
+                >MORE</a
+              >
+            </div>
+          </a>
+          <a
+            href="http://life.china.com.cn/2020-09/29/content_41315174.htm"
+            target="_blank"
+            class="news_item"
+          >
+            <img
+              src="https://popwebsite.paquapp.com/cms/news/jZ7KhyxF8T2dcE57anD2Gn2mkR39dKW8.jpg"
+              alt=""
+            />
+            <div class="pop_1">
+              <div class="news_title">
+                泡泡玛特首次举办粉丝沙龙，央美教授分享潮玩故事
+              </div>
+              <p class="news_desc">
+                9月27日,在泡泡玛特浦项中心体验店,一场别开生面的粉丝沙龙活动与大家见面。
+              </p>
+              <a
+                href="http://life.ynet.com/2020/10/12/2914725t978.html"
+                class="el-icon-arrow-right"
+                >MORE</a
+              >
+            </div>
+          </a>
+          <a
+            href="https://www.toutiao.com/i6875243621168185869/"
+            target="_blank"
+            class="news_item"
+          >
+            <img
+              src="https://popwebsite.paquapp.com/cms/news/FhkAM7XpfdnKKK02isb6wc3fj6ZF73wb.webp"
+              alt=""
+            />
+            <div class="pop_1">
+              <div class="news_title">
+                《国家宝藏》回归之际，潮玩先行
+              </div>
+              <p class="news_desc">
+                泡泡玛特与国家宝藏合作的最后一款潮玩——铜奔马也如约而至，至此，铜奔马、洛神、李白、格萨尔唐卡均已面世。
+              </p>
+              <a
+                href="http://life.ynet.com/2020/10/12/2914725t978.html"
+                class="el-icon-arrow-right"
+                >MORE</a
+              >
+            </div>
+          </a>
+          <a
+            href="http://paper.people.com.cn/rmrbhwb/html/2020-09/18/content_2009561.htm"
+            target="_blank"
+            class="news_item"
+          >
+            <img
+              src="https://popwebsite.paquapp.com/cms/news/rj2y58weH75et0476ydCr6NCdws5arfh.jpg"
+              alt=""
+            />
+            <div class="pop_1">
+              <div class="news_title">
+                自主创新的中国潮玩走向世界
+              </div>
+              <p class="news_desc">
+                潮玩已不再是少部分人的兴趣爱好，甚至已上升为一种精神消费，表现出强大的消费潜力。
+              </p>
+              <a
+                href="http://life.ynet.com/2020/10/12/2914725t978.html"
+                class="el-icon-arrow-right"
+                >MORE</a
+              >
+            </div>
+          </a>
+        </div>
+      </div>
+
+      <!-- 底部小标题 -->
+      <a class="floot_tltle ">
+        MORE
+        <i class="el-icon-arrow-right"></i>
+      </a>
+
+      <div class="floot">
+        <a href="https://www.popmart.com/home"></a>
+        <div class="footer-con">
+          <div class="footer-right-icon">
+            <a href="https://wx.qq.com/" class="footer_icon1"> </a>
+            <a href="https://passport.weibo.com/" class="footer_icon2"></a>
+            <a
+              href="https://www.instagram.com/popmartglobal/"
+              class="footer_icon3"
+            ></a>
+            <a
+              href="https://www.facebook.com/popmartglobal"
+              class="footer_icon4"
+            ></a>
+            <a href="https://www.youtube.com/" class="footer_icon5"></a>
+            <a
+              href="https://twitter.com/POPMARTGlobal"
+              class="footer_icon6"
+            ></a>
+          </div>
+
+          <!-- 加入我们 -->
+          <div class="footer-con-box">
+            <a href="##">加入我们</a>
+            <i class="left"></i>
+            <a href="##">联系我们</a>
+          </div>
+          <!-- 地址 -->
+          <div class="footer-con-site">
+            <div>
+              北京市朝阳区望京东园四区 13号楼 -4 至 33 层 101 内 A 座 36 层 3606
+              室
+            </div>
+            <i class="left"></i>
+            <div class="rigth">
+              4000232233@POPMART.COM
+            </div>
+          </div>
+          <div class="footer-con-site1">
+            <div class="floot_floot">
+             COPYRIGHT © 泡泡玛特 POPMART.COM ALL RIGHTS RESERVED
+            </div>
+            <i class="left"></i>
+            <div class="rigth">
+              京 ICP 备 11028233 号 - 3
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
 <script>
+//引入swpier
+import Swiper from "swiper";
+//引入样式
+import "./home.css";
 export default {
   name: "Home",
-  data() {
-    return {};
+  mounted() {
+    var mySwiper = new Swiper(".swiper-container", {
+      // direction: "vertical", // 垂直切换选项
+      loop: true, // 循环模式选项
+      // 如果需要分页器
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+        renderBullet: function(index, className) {
+          return (
+            `<image src="https://popwebsite.paquapp.com/popmartwww/pc/popularProduct/ipAvatar${23 +
+              index}.png " class="` +
+            className +
+            '">' +
+            "</image>"
+          );
+        },
+      },
+
+      // 如果需要前进后退按钮
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+
+      // 如果需要滚动条
+      scrollbar: {
+        el: ".swiper-scrollbar",
+      },
+    });
   },
-  components: {},
 };
 </script>
-
-<style lang="less" scoped>
-/* div{
-    font-size:100px
-  } */
-</style>
+<style></style>
